5c797de08cefd610b72023b5c3e1c4631014b847
Bruno Basto <bruno.basto@liferay.com>
2016-02-03 19:12:58 -0300

    LPS-62767 Only enabling form submission when told so.

--- modules/frontend/frontend-js-spa-web/src/main/resources/META-INF/resources/liferay/init.es.js
+++ modules/frontend/frontend-js-spa-web/src/main/resources/META-INF/resources/liferay/init.es.js
@@ -13,7 +13,7 @@ import Utils from './util/Utils.es';
 let app = new App();
 
 app.setBasePath(Utils.getBasePath());
-app.setFormSelector('form:not([data-senna-off])');
+app.setFormSelector('form.senna-enabled');
 app.setLinkSelector('a:not(.portlet-icon-back):not([data-navigation]):not([data-resource-href]):not([target="_blank"])');
 
 Utils.getSurfaceIds().forEach(
