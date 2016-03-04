8ab17fc7571da2e6e8ecd2db6d7e6cfe54a59c8d
Kenji Heigel <kenji.heigel@liferay.com>
2016-03-04 10:07:13 -0800

    Revert "LPS-63918 Inject module path"
    This reverts commit d4050dde9d1392e7cc150175b49aa771456ceb90.

--- modules/apps/platform/frontend/frontend-js-bundle-config-extender/src/main/java/com/liferay/frontend/js/bundle/config/extender/JSBundleConfigServlet.java
+++ modules/apps/platform/frontend/frontend-js-bundle-config-extender/src/main/java/com/liferay/frontend/js/bundle/config/extender/JSBundleConfigServlet.java
@@ -16,7 +16,6 @@ package com.liferay.frontend.js.bundle.config.extender;
 
 import com.liferay.frontend.js.bundle.config.extender.JSBundleConfigTracker.JSConfig;
 import com.liferay.portal.kernel.util.ContentTypes;
-import com.liferay.portal.kernel.util.PortalUtil;
 import com.liferay.portal.kernel.util.StreamUtil;
 
 import java.io.IOException;
@@ -95,11 +94,6 @@ public class JSBundleConfigServlet extends HttpServlet {
 
 					ServletContext servletContext =
 						jsConfig.getServletContext();
-
-					servletOutputStream.println(
-						"var MODULE_PATH = '" +
-							PortalUtil.getPathContext() +
-								servletContext.getContextPath() + "/';");
 
 					StreamUtil.transfer(
 						inputStream, servletOutputStream, false);
