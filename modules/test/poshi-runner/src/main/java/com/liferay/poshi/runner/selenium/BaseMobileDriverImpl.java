0aeb71054336d73ca83eace53c0b83c08ff39aa4
Kenji Heigel <kenji.heigel@liferay.com>
2016-03-29 11:37:51 -0700

    implement MobileDriver in BaseMobileDriverImpl

--- modules/test/poshi-runner/src/main/java/com/liferay/poshi/runner/selenium/BaseMobileDriverImpl.java
+++ modules/test/poshi-runner/src/main/java/com/liferay/poshi/runner/selenium/BaseMobileDriverImpl.java
@@ -30,13 +30,11 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.html5.Location;
 import org.openqa.selenium.remote.Response;
 
-import org.w3c.dom.Node;
-
 /**
  * @author Kenji Heigel
  */
 public abstract class BaseMobileDriverImpl
-	extends BaseWebDriverImpl {
+	extends BaseWebDriverImpl implements MobileDriver {
 
 	public BaseMobileDriverImpl(String browserURL, WebDriver webDriver) {
 		super(browserURL, webDriver);
@@ -62,6 +60,51 @@ public abstract class BaseMobileDriverImpl
 		}
 	}
 
+	@Override
+	public void closeApp() {
+		_mobileDriver.closeApp();
+	}
+
+	@Override
+	public WebDriver context(String name) {
+		return _mobileDriver.context(name);
+	}
+
+	@Override
+	public Response execute(String driverCommand, Map<String, ?> parameters) {
+		return _mobileDriver.execute(driverCommand, parameters);
+	}
+
+	@Override
+	public WebElement findElementByAccessibilityId(String using) {
+		return _mobileDriver.findElementByAccessibilityId(using);
+	}
+
+	@Override
+	public List<WebElement> findElementsByAccessibilityId(String using) {
+		return _mobileDriver.findElementsByAccessibilityId(using);
+	}
+
+	@Override
+	public String getAppStrings() {
+		return _mobileDriver.getAppStrings();
+	}
+
+	@Override
+	public String getAppStrings(String language) {
+		return _mobileDriver.getAppStrings(language);
+	}
+
+	@Override
+	public String getContext() {
+		return _mobileDriver.getContext();
+	}
+
+	@Override
+	public Set<String> getContextHandles() {
+		return _mobileDriver.getContextHandles();
+	}
+
 	public String getElementValue(String locator, String timeout)
 		throws Exception {
 
@@ -77,6 +120,11 @@ public abstract class BaseMobileDriverImpl
 		}
 
 		return webElement.getAttribute("value");
+	}
+
+	@Override
+	public ScreenOrientation getOrientation() {
+		return _mobileDriver.getOrientation();
 	}
 
 	public String getText(String locator, String timeout) throws Exception {
@@ -96,6 +144,21 @@ public abstract class BaseMobileDriverImpl
 		text = text.trim();
 
 		return text.replace("\n", " ");
+	}
+
+	@Override
+	public void hideKeyboard() {
+		_mobileDriver.hideKeyboard();
+	}
+
+	@Override
+	public void installApp(String appPath) {
+		_mobileDriver.installApp(appPath);
+	}
+
+	@Override
+	public boolean isAppInstalled(String bundleId) {
+		return _mobileDriver.isAppInstalled(bundleId);
 	}
 
 	@Override
@@ -153,12 +216,114 @@ public abstract class BaseMobileDriverImpl
 	}
 
 	@Override
+	public void launchApp() {
+		_mobileDriver.launchApp();
+	}
+
+	@Override
+	public Location location() {
+		return _mobileDriver.location();
+	}
+
+	@Override
 	public void mouseOver(String locator) {
+	}
+
+	@Override
+	public void performMultiTouchAction(MultiTouchAction multiAction) {
+		_mobileDriver.performMultiTouchAction(multiAction);
+	}
+
+	@Override
+	public TouchAction performTouchAction(TouchAction touchAction) {
+		return _mobileDriver.performTouchAction(touchAction);
+	}
+
+	@Override
+	public void pinch(int x, int y) {
+		_mobileDriver.pinch(x, y);
+	}
+
+	@Override
+	public void pinch(WebElement el) {
+		_mobileDriver.pinch(el);
+	}
+
+	@Override
+	public byte[] pullFile(String remotePath) {
+		return _mobileDriver.pullFile(remotePath);
+	}
+
+	@Override
+	public byte[] pullFolder(String remotePath) {
+		return _mobileDriver.pullFolder(remotePath);
+	}
+
+	@Override
+	public void removeApp(String bundleId) {
+		_mobileDriver.removeApp(bundleId);
+	}
+
+	@Override
+	public void resetApp() {
+		_mobileDriver.resetApp();
+	}
+
+	@Override
+	public void rotate(ScreenOrientation orientation) {
+		_mobileDriver.rotate(orientation);
+	}
+
+	@Override
+	public void runAppInBackground(int seconds) {
+		_mobileDriver.runAppInBackground(seconds);
+	}
+
+	@Override
+	public WebElement scrollTo(String text) {
+		return _mobileDriver.scrollTo(text);
+	}
+
+	@Override
+	public WebElement scrollToExact(String text) {
+		return _mobileDriver.scrollToExact(text);
 	}
 
 	@Override
 	public void scrollWebElementIntoView(String locator) throws Exception {
 		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public void setLocation(Location location) {
+		_mobileDriver.setLocation(location);
+	}
+
+	@Override
+	public void swipe(
+		int startx, int starty, int endx, int endy, int duration) {
+
+		_mobileDriver.swipe(startx, starty, endx, endy, duration);
+	}
+
+	@Override
+	public void tap(int fingers, int x, int y, int duration) {
+		_mobileDriver.tap(fingers, x, y, duration);
+	}
+
+	@Override
+	public void tap(int fingers, WebElement element, int duration) {
+		_mobileDriver.tap(fingers, element, duration);
+	}
+
+	@Override
+	public void zoom(int x, int y) {
+		_mobileDriver.zoom(x, y);
+	}
+
+	@Override
+	public void zoom(WebElement el) {
+		_mobileDriver.zoom(el);
 	}
 
 	protected void swipeWebElementIntoView(String locator) {
@@ -178,6 +343,7 @@ public abstract class BaseMobileDriverImpl
 	private static final String _TEST_DEPENDENCIES_DIR_NAME =
 		PropsValues.TEST_DEPENDENCIES_DIR_NAME;
 
-	private String _primaryTestSuiteName;
+	private final MobileDriver _mobileDriver;
+	private final String _primaryTestSuiteName;
 
 }
