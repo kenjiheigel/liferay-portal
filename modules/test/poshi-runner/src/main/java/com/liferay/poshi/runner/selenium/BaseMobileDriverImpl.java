4b7e2d1b26065e37e851feadadbec62b96734bcc
Kenji Heigel <kenji.heigel@liferay.com>
2016-08-11 17:34:07 -0700

    LRQA-27177 Remove methods from BaseMobileDriverImpl that are unsupported across both WebDriver and MobileDriver

--- modules/test/poshi-runner/src/main/java/com/liferay/poshi/runner/selenium/BaseMobileDriverImpl.java
+++ modules/test/poshi-runner/src/main/java/com/liferay/poshi/runner/selenium/BaseMobileDriverImpl.java
@@ -36,45 +36,8 @@ public abstract class BaseMobileDriverImpl extends MobileDriverWrapper {
 	}
 
 	@Override
-	public void addCustomRequestHeader(String key, String value) {
-		throw new UnsupportedOperationException();
-	}
-
-	@Override
-	public void addLocationStrategy(
-		String strategyName, String functionDefinition) {
-
-		throw new UnsupportedOperationException();
-	}
-
-	@Override
-	public void addScript(String script, String scriptTagId) {
-		throw new UnsupportedOperationException();
-	}
-
-	@Override
 	public void addSelection(String locator, String optionLocator) {
 		WebDriverHelper.addSelection(this, locator, optionLocator);
-	}
-
-	@Override
-	public void allowNativeXpath(String allow) {
-		throw new UnsupportedOperationException();
-	}
-
-	@Override
-	public void altKeyDown() {
-		throw new UnsupportedOperationException();
-	}
-
-	@Override
-	public void altKeyUp() {
-		throw new UnsupportedOperationException();
-	}
-
-	@Override
-	public void answerOnNextPrompt(String answer) {
-		throw new UnsupportedOperationException();
 	}
 
 	@Override
@@ -291,53 +254,8 @@ public abstract class BaseMobileDriverImpl extends MobileDriverWrapper {
 	}
 
 	@Override
-	public void assignId(String locator, String identifier) {
-		throw new UnsupportedOperationException();
-	}
-
-	@Override
-	public void attachFile(String fieldLocator, String fileLocator) {
-		throw new UnsupportedOperationException();
-	}
-
-	@Override
-	public void captureEntirePageScreenshot(String fileName, String kwargs) {
-		throw new UnsupportedOperationException();
-	}
-
-	@Override
-	public String captureEntirePageScreenshotToString(String kwargs) {
-		throw new UnsupportedOperationException();
-	}
-
-	@Override
-	public String captureNetworkTraffic(String type) {
-		throw new UnsupportedOperationException();
-	}
-
-	@Override
-	public void captureScreenshot(String fileName) {
-		throw new UnsupportedOperationException();
-	}
-
-	@Override
-	public String captureScreenshotToString() {
-		throw new UnsupportedOperationException();
-	}
-
-	@Override
 	public void check(String locator) {
 		WebDriverHelper.check(this, locator);
-	}
-
-	@Override
-	public void chooseCancelOnNextConfirmation() {
-		throw new UnsupportedOperationException();
-	}
-
-	@Override
-	public void chooseOkOnNextConfirmation() {
-		throw new UnsupportedOperationException();
 	}
 
 	@Override
@@ -393,26 +311,6 @@ public abstract class BaseMobileDriverImpl extends MobileDriverWrapper {
 	}
 
 	@Override
-	public void contextMenu(String locator) {
-		throw new UnsupportedOperationException();
-	}
-
-	@Override
-	public void contextMenuAt(String locator, String coordString) {
-		throw new UnsupportedOperationException();
-	}
-
-	@Override
-	public void controlKeyDown() {
-		throw new UnsupportedOperationException();
-	}
-
-	@Override
-	public void controlKeyUp() {
-		throw new UnsupportedOperationException();
-	}
-
-	@Override
 	public void copyText(String locator) throws Exception {
 		throw new UnsupportedOperationException();
 	}
@@ -423,28 +321,8 @@ public abstract class BaseMobileDriverImpl extends MobileDriverWrapper {
 	}
 
 	@Override
-	public void createCookie(String nameValuePair, String optionsString) {
-		throw new UnsupportedOperationException();
-	}
-
-	@Override
 	public void deleteAllEmails() throws Exception {
 		LiferaySeleniumHelper.deleteAllEmails();
-	}
-
-	@Override
-	public void deleteAllVisibleCookies() {
-		throw new UnsupportedOperationException();
-	}
-
-	@Override
-	public void deleteCookie(String name, String optionsString) {
-		throw new UnsupportedOperationException();
-	}
-
-	@Override
-	public void deselectPopUp() {
-		throw new UnsupportedOperationException();
 	}
 
 	@Override
@@ -486,58 +364,13 @@ public abstract class BaseMobileDriverImpl extends MobileDriverWrapper {
 	}
 
 	@Override
-	public void fireEvent(String locator, String eventName) {
-		throw new UnsupportedOperationException();
-	}
-
-	@Override
-	public void focus(String locator) {
-		throw new UnsupportedOperationException();
-	}
-
-	@Override
 	public String getAlert() {
-		throw new UnsupportedOperationException();
-	}
-
-	@Override
-	public String[] getAllButtons() {
-		throw new UnsupportedOperationException();
-	}
-
-	@Override
-	public String[] getAllFields() {
-		throw new UnsupportedOperationException();
-	}
-
-	@Override
-	public String[] getAllLinks() {
-		throw new UnsupportedOperationException();
-	}
-
-	@Override
-	public String[] getAllWindowIds() {
-		throw new UnsupportedOperationException();
-	}
-
-	@Override
-	public String[] getAllWindowNames() {
-		throw new UnsupportedOperationException();
-	}
-
-	@Override
-	public String[] getAllWindowTitles() {
 		throw new UnsupportedOperationException();
 	}
 
 	@Override
 	public String getAttribute(String attributeLocator) {
 		return WebDriverHelper.getAttribute(this, attributeLocator);
-	}
-
-	@Override
-	public String[] getAttributeFromAllWindows(String attributeName) {
-		throw new UnsupportedOperationException();
 	}
 
 	@Override
@@ -548,21 +381,6 @@ public abstract class BaseMobileDriverImpl extends MobileDriverWrapper {
 	@Override
 	public String getConfirmation() {
 		return WebDriverHelper.getConfirmation(this);
-	}
-
-	@Override
-	public String getCookie() {
-		throw new UnsupportedOperationException();
-	}
-
-	@Override
-	public String getCookieByName(String name) {
-		throw new UnsupportedOperationException();
-	}
-
-	@Override
-	public Number getCssCount(String css) {
-		throw new UnsupportedOperationException();
 	}
 
 	@Override
@@ -591,28 +409,8 @@ public abstract class BaseMobileDriverImpl extends MobileDriverWrapper {
 	}
 
 	@Override
-	public Number getCursorPosition(String locator) {
-		throw new UnsupportedOperationException();
-	}
-
-	@Override
 	public int getElementHeight(String locator) {
 		return WebDriverHelper.getElementHeight(this, locator);
-	}
-
-	@Override
-	public Number getElementIndex(String locator) {
-		throw new UnsupportedOperationException();
-	}
-
-	@Override
-	public Number getElementPositionLeft(String locator) {
-		throw new UnsupportedOperationException();
-	}
-
-	@Override
-	public Number getElementPositionTop(String locator) {
-		throw new UnsupportedOperationException();
 	}
 
 	@Override
@@ -658,11 +456,6 @@ public abstract class BaseMobileDriverImpl extends MobileDriverWrapper {
 	}
 
 	@Override
-	public String getExpression(String expression) {
-		throw new UnsupportedOperationException();
-	}
-
-	@Override
 	public String getFirstNumber(String locator) {
 		throw new UnsupportedOperationException();
 	}
@@ -695,16 +488,6 @@ public abstract class BaseMobileDriverImpl extends MobileDriverWrapper {
 	}
 
 	@Override
-	public String getLog() {
-		throw new UnsupportedOperationException();
-	}
-
-	@Override
-	public Number getMouseSpeed() {
-		throw new UnsupportedOperationException();
-	}
-
-	@Override
 	public String getNumberDecrement(String value) {
 		return LiferaySeleniumHelper.getNumberDecrement(value);
 	}
@@ -725,31 +508,6 @@ public abstract class BaseMobileDriverImpl extends MobileDriverWrapper {
 	}
 
 	@Override
-	public String getPrompt() {
-		throw new UnsupportedOperationException();
-	}
-
-	@Override
-	public String getSelectedId(String selectLocator) {
-		throw new UnsupportedOperationException();
-	}
-
-	@Override
-	public String[] getSelectedIds(String selectLocator) {
-		throw new UnsupportedOperationException();
-	}
-
-	@Override
-	public String getSelectedIndex(String selectLocator) {
-		throw new UnsupportedOperationException();
-	}
-
-	@Override
-	public String[] getSelectedIndexes(String selectLocator) {
-		throw new UnsupportedOperationException();
-	}
-
-	@Override
 	public String getSelectedLabel(String selectLocator) {
 		return getSelectedLabel(selectLocator, null);
 	}
@@ -764,33 +522,8 @@ public abstract class BaseMobileDriverImpl extends MobileDriverWrapper {
 	}
 
 	@Override
-	public String getSelectedValue(String selectLocator) {
-		throw new UnsupportedOperationException();
-	}
-
-	@Override
-	public String[] getSelectedValues(String selectLocator) {
-		throw new UnsupportedOperationException();
-	}
-
-	@Override
-	public String[] getSelectOptions(String selectLocator) {
-		throw new UnsupportedOperationException();
-	}
-
-	@Override
 	public String getSikuliImagesDirName() {
 		return _SIKULI_IMAGES_DIR_NAME;
-	}
-
-	@Override
-	public String getSpeed() {
-		throw new UnsupportedOperationException();
-	}
-
-	@Override
-	public String getTable(String tableCellAddress) {
-		throw new UnsupportedOperationException();
 	}
 
 	@Override
@@ -828,30 +561,6 @@ public abstract class BaseMobileDriverImpl extends MobileDriverWrapper {
 	}
 
 	@Override
-	public String getValue(String locator) {
-		throw new UnsupportedOperationException();
-	}
-
-	@Override
-	public boolean getWhetherThisFrameMatchFrameExpression(
-		String currentFrameString, String target) {
-
-		throw new UnsupportedOperationException();
-	}
-
-	@Override
-	public boolean getWhetherThisWindowMatchWindowExpression(
-		String currentWindowString, String target) {
-
-		throw new UnsupportedOperationException();
-	}
-
-	@Override
-	public Number getXpathCount(String xPath) {
-		throw new UnsupportedOperationException();
-	}
-
-	@Override
 	public void goBack() {
 		WebDriverHelper.goBack(this);
 	}
@@ -861,16 +570,6 @@ public abstract class BaseMobileDriverImpl extends MobileDriverWrapper {
 		goBack();
 
 		waitForPageToLoad("30000");
-	}
-
-	@Override
-	public void highlight(String locator) {
-		throw new UnsupportedOperationException();
-	}
-
-	@Override
-	public void ignoreAttributesWithoutValue(String ignore) {
-		throw new UnsupportedOperationException();
 	}
 
 	@Override
@@ -896,16 +595,6 @@ public abstract class BaseMobileDriverImpl extends MobileDriverWrapper {
 	@Override
 	public boolean isConfirmation(String pattern) {
 		return LiferaySeleniumHelper.isConfirmation(this, pattern);
-	}
-
-	@Override
-	public boolean isConfirmationPresent() {
-		throw new UnsupportedOperationException();
-	}
-
-	@Override
-	public boolean isCookiePresent(String name) {
-		throw new UnsupportedOperationException();
 	}
 
 	@Override
@@ -988,18 +677,8 @@ public abstract class BaseMobileDriverImpl extends MobileDriverWrapper {
 	}
 
 	@Override
-	public boolean isOrdered(String locator1, String locator2) {
-		throw new UnsupportedOperationException();
-	}
-
-	@Override
 	public boolean isPartialText(String locator, String value) {
 		return WebDriverHelper.isPartialText(this, locator, value);
-	}
-
-	@Override
-	public boolean isPromptPresent() {
-		throw new UnsupportedOperationException();
 	}
 
 	@Override
@@ -1010,11 +689,6 @@ public abstract class BaseMobileDriverImpl extends MobileDriverWrapper {
 	@Override
 	public boolean isSikuliImagePresent(String image) throws Exception {
 		return LiferaySeleniumHelper.isSikuliImagePresent(this, image);
-	}
-
-	@Override
-	public boolean isSomethingSelected(String selectLocator) {
-		throw new UnsupportedOperationException();
 	}
 
 	@Override
@@ -1086,22 +760,12 @@ public abstract class BaseMobileDriverImpl extends MobileDriverWrapper {
 	}
 
 	@Override
-	public void keyDownNative(String keycode) {
-		throw new UnsupportedOperationException();
-	}
-
-	@Override
 	public void keyPress(String locator, String keySequence) {
 		throw new UnsupportedOperationException();
 	}
 
 	@Override
 	public void keyPressAndWait(String locator, String keySequence) {
-		throw new UnsupportedOperationException();
-	}
-
-	@Override
-	public void keyPressNative(String keycode) {
 		throw new UnsupportedOperationException();
 	}
 
@@ -1116,23 +780,8 @@ public abstract class BaseMobileDriverImpl extends MobileDriverWrapper {
 	}
 
 	@Override
-	public void keyUpNative(String keycode) {
-		throw new UnsupportedOperationException();
-	}
-
-	@Override
 	public void makeVisible(String locator) {
 		WebDriverHelper.makeVisible(this, locator);
-	}
-
-	@Override
-	public void metaKeyDown() {
-		throw new UnsupportedOperationException();
-	}
-
-	@Override
-	public void metaKeyUp() {
-		throw new UnsupportedOperationException();
 	}
 
 	@Override
@@ -1142,16 +791,6 @@ public abstract class BaseMobileDriverImpl extends MobileDriverWrapper {
 
 	@Override
 	public void mouseDownAt(String locator, String coordString) {
-		throw new UnsupportedOperationException();
-	}
-
-	@Override
-	public void mouseDownRight(String locator) {
-		throw new UnsupportedOperationException();
-	}
-
-	@Override
-	public void mouseDownRightAt(String locator, String coordString) {
 		throw new UnsupportedOperationException();
 	}
 
@@ -1190,23 +829,8 @@ public abstract class BaseMobileDriverImpl extends MobileDriverWrapper {
 	}
 
 	@Override
-	public void mouseUpRight(String locator) {
-		throw new UnsupportedOperationException();
-	}
-
-	@Override
-	public void mouseUpRightAt(String locator, String coordString) {
-		throw new UnsupportedOperationException();
-	}
-
-	@Override
 	public void open(String url) {
 		WebDriverHelper.open(this, url);
-	}
-
-	@Override
-	public void open(String url, String ignoreResponseCode) {
-		throw new UnsupportedOperationException();
 	}
 
 	@Override
@@ -1239,33 +863,8 @@ public abstract class BaseMobileDriverImpl extends MobileDriverWrapper {
 	}
 
 	@Override
-	public void removeAllSelections(String locator) {
-		throw new UnsupportedOperationException();
-	}
-
-	@Override
-	public void removeScript(String scriptTagId) {
-		throw new UnsupportedOperationException();
-	}
-
-	@Override
-	public void removeSelection(String locator, String optionLocator) {
-		throw new UnsupportedOperationException();
-	}
-
-	@Override
 	public void replyToEmail(String to, String body) throws Exception {
 		LiferaySeleniumHelper.replyToEmail(this, to, body);
-	}
-
-	@Override
-	public String retrieveLastRemoteControlLogs() {
-		throw new UnsupportedOperationException();
-	}
-
-	@Override
-	public void rollup(String rollupName, String kwargs) {
-		throw new UnsupportedOperationException();
 	}
 
 	@Override
@@ -1382,21 +981,6 @@ public abstract class BaseMobileDriverImpl extends MobileDriverWrapper {
 	}
 
 	@Override
-	public void setBrowserLogLevel(String logLevel) {
-		throw new UnsupportedOperationException();
-	}
-
-	@Override
-	public void setContext(String context) {
-		throw new UnsupportedOperationException();
-	}
-
-	@Override
-	public void setCursorPosition(String locator, String position) {
-		throw new UnsupportedOperationException();
-	}
-
-	@Override
 	public void setDefaultTimeout() {
 	}
 
@@ -1406,23 +990,8 @@ public abstract class BaseMobileDriverImpl extends MobileDriverWrapper {
 	}
 
 	@Override
-	public void setExtensionJs(String extensionJs) {
-		throw new UnsupportedOperationException();
-	}
-
-	@Override
-	public void setMouseSpeed(String pixels) {
-		throw new UnsupportedOperationException();
-	}
-
-	@Override
 	public void setPrimaryTestSuiteName(String primaryTestSuiteName) {
 		_primaryTestSuiteName = primaryTestSuiteName;
-	}
-
-	@Override
-	public void setSpeed(String value) {
-		throw new UnsupportedOperationException();
 	}
 
 	@Override
@@ -1437,31 +1006,6 @@ public abstract class BaseMobileDriverImpl extends MobileDriverWrapper {
 
 	@Override
 	public void setWindowSize(String coordString) {
-		throw new UnsupportedOperationException();
-	}
-
-	@Override
-	public void shiftKeyDown() {
-		throw new UnsupportedOperationException();
-	}
-
-	@Override
-	public void shiftKeyUp() {
-		throw new UnsupportedOperationException();
-	}
-
-	@Override
-	public void showContextualBanner() {
-		throw new UnsupportedOperationException();
-	}
-
-	@Override
-	public void showContextualBanner(String className, String methodName) {
-		throw new UnsupportedOperationException();
-	}
-
-	@Override
-	public void shutDownSeleniumServer() {
 		throw new UnsupportedOperationException();
 	}
 
@@ -1546,21 +1090,6 @@ public abstract class BaseMobileDriverImpl extends MobileDriverWrapper {
 	}
 
 	@Override
-	public void start() {
-		throw new UnsupportedOperationException();
-	}
-
-	@Override
-	public void start(Object optionsObject) {
-		throw new UnsupportedOperationException();
-	}
-
-	@Override
-	public void start(String optionsString) {
-		throw new UnsupportedOperationException();
-	}
-
-	@Override
 	public void startLogger() {
 	}
 
@@ -1571,11 +1100,6 @@ public abstract class BaseMobileDriverImpl extends MobileDriverWrapper {
 
 	@Override
 	public void stopLogger() {
-	}
-
-	@Override
-	public void submit(String formLocator) {
-		throw new UnsupportedOperationException();
 	}
 
 	@Override
@@ -1640,16 +1164,6 @@ public abstract class BaseMobileDriverImpl extends MobileDriverWrapper {
 	}
 
 	@Override
-	public void useXpathLibrary(String libraryName) {
-		throw new UnsupportedOperationException();
-	}
-
-	@Override
-	public void waitForCondition(String script, String timeout) {
-		throw new UnsupportedOperationException();
-	}
-
-	@Override
 	public void waitForConfirmation(String pattern) throws Exception {
 		LiferaySeleniumHelper.waitForConfirmation(this, pattern);
 	}
@@ -1662,11 +1176,6 @@ public abstract class BaseMobileDriverImpl extends MobileDriverWrapper {
 	@Override
 	public void waitForElementPresent(String locator) throws Exception {
 		LiferaySeleniumHelper.waitForElementPresent(this, locator);
-	}
-
-	@Override
-	public void waitForFrameToLoad(String frameAddress, String timeout) {
-		throw new UnsupportedOperationException();
 	}
 
 	@Override
@@ -1747,16 +1256,6 @@ public abstract class BaseMobileDriverImpl extends MobileDriverWrapper {
 	@Override
 	public void waitForVisible(String locator) throws Exception {
 		LiferaySeleniumHelper.waitForVisible(this, locator);
-	}
-
-	@Override
-	public void windowFocus() {
-		throw new UnsupportedOperationException();
-	}
-
-	@Override
-	public void windowMaximize() {
-		throw new UnsupportedOperationException();
 	}
 
 	@Override
