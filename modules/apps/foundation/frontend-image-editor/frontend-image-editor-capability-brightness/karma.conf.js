8b68d46d5beb579b32f2ef460e0f1c63220cf1ce
Kenji Heigel <kenji.heigel@liferay.com>
2016-08-31 17:18:26 -0700

    LRQA-27491 Add Junit reporting and Firefox and use custom karma.conf.js

--- modules/apps/foundation/frontend-image-editor/frontend-image-editor-capability-brightness/karma.conf.js
+++ modules/apps/foundation/frontend-image-editor/frontend-image-editor-capability-brightness/karma.conf.js
@@ -1,9 +1,55 @@
 'use strict';
 
-var metalKarmaConfig = require('metal-karma-config');
+var babelPresetMetal = require('babel-preset-metal');
+var karmaBabelPreprocessor = require('karma-babel-preprocessor');
+var karmaChai = require('karma-chai');
+var karmaFirefoxLauncher = require('karma-firefox-launcher');
+var karmaCommonJs = require('karma-commonjs');
+var karmaJunitReporter = require('karma-junit-reporter');
+var karmaMocha = require('karma-mocha');
+var karmaSinon = require('karma-sinon');
+
+var babelOptions = {
+	presets: [babelPresetMetal],
+	sourceMap: 'both'
+};
 
 module.exports = function(config) {
-	metalKarmaConfig(config);
+	config.set({
+		plugins: [
+			karmaBabelPreprocessor,
+			karmaChai,
+			karmaFirefoxLauncher,
+			karmaCommonJs,
+			karmaJunitReporter,
+			karmaMocha,
+			karmaSinon
+		],
+
+		frameworks: ['mocha', 'chai', 'sinon', 'commonjs'],
+
+		files: [
+			'node_modules/metal-soy-bundle/build/bundle.js',
+			'node_modules/html2incdom/src/*.js',
+			'node_modules/metal*/src/**/*.js',
+			'src/**/*.js',
+			'test/**/*.js'
+		],
+
+		preprocessors: {
+			'src/**/*.js': ['babel', 'commonjs'],
+			'node_modules/html2incdom/src/*.js': ['babel', 'commonjs'],
+			'node_modules/metal-soy-bundle/build/bundle.js': ['babel', 'commonjs'],
+			'node_modules/metal*/src/**/*.js': ['babel', 'commonjs'],
+			'test/**/*.js': ['babel', 'commonjs']
+		},
+
+		reporters: ['junit', 'progress'],
+
+		browsers: ['Firefox'],
+
+		babelPreprocessor: {options: babelOptions}
+	});
 
 	config.files.push(
 		'node_modules/resemblejs/resemble.js',
