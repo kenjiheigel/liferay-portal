15e1266d0de0b61a32f8ed9747da7ce62004c934
pyoo47 <peter.yoo@liferay.com>
2017-06-05 15:06:13 -0700

    LRQA-32801 Don't need to create a new stack to get the stack trace.

--- modules/test/poshi-runner/poshi-runner/src/main/java/com/liferay/poshi/runner/PoshiRunnerStackTraceUtil.java
+++ modules/test/poshi-runner/poshi-runner/src/main/java/com/liferay/poshi/runner/PoshiRunnerStackTraceUtil.java
@@ -74,18 +74,14 @@ public final class PoshiRunnerStackTraceUtil {
 			sb.append(msg);
 		}
 
-		Stack<String> stackTrace = new Stack<>();
-
-		stackTrace.addAll(_stackTrace);
-
 		sb.append("\n");
 		sb.append(_filePaths.peek());
 		sb.append(":");
 		sb.append(_currentElement.attributeValue("line-number"));
 
-		while (!stackTrace.isEmpty()) {
+		for (int i = 0; i < _stackTrace.size(); i++) {
 			sb.append("\n");
-			sb.append(stackTrace.pop());
+			sb.append(_stackTrace.get(i));
 		}
 
 		sb.append("\n");
