8f24c2f3a5d0b5ce8dbef89f05ca3ae1385cd5e5
Kenji Heigel <kenji.heigel@liferay.com>
2018-01-22 17:44:57 -0800

    LRQA-37675 Remove poshi test result specific methods from TestResult interface

--- modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/BaseTestResult.java
+++ modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/BaseTestResult.java
@@ -78,16 +78,6 @@ public class BaseTestResult implements TestResult {
 	}
 
 	@Override
-	public String getConsoleOutputURL(String testrayLogsURL) {
-		StringBuilder sb = new StringBuilder();
-
-		sb.append(testrayLogsURL);
-		sb.append("/jenkins-console.txt.gz");
-
-		return sb.toString();
-	}
-
-	@Override
 	public String getDisplayName() {
 		if (testName.startsWith("test[")) {
 			return testName.substring(5, testName.length() - 1);
@@ -128,50 +118,8 @@ public class BaseTestResult implements TestResult {
 	}
 
 	@Override
-	public String getLiferayLogURL(String testrayLogsURL) {
-		StringBuilder sb = new StringBuilder();
-
-		String name = getDisplayName();
-
-		sb.append(testrayLogsURL);
-		sb.append("/");
-		sb.append(name.replace("#", "_"));
-		sb.append("/liferay-log.txt.gz");
-
-		return sb.toString();
-	}
-
-	@Override
 	public String getPackageName() {
 		return packageName;
-	}
-
-	@Override
-	public String getPoshiReportURL(String testrayLogsURL) {
-		StringBuilder sb = new StringBuilder();
-
-		String name = getDisplayName();
-
-		sb.append(testrayLogsURL);
-		sb.append("/");
-		sb.append(name.replace("#", "_"));
-		sb.append("/index.html.gz");
-
-		return sb.toString();
-	}
-
-	@Override
-	public String getPoshiSummaryURL(String testrayLogsURL) {
-		StringBuilder sb = new StringBuilder();
-
-		String name = getDisplayName();
-
-		sb.append(testrayLogsURL);
-		sb.append("/");
-		sb.append(name.replace("#", "_"));
-		sb.append("/summary.html.gz");
-
-		return sb.toString();
 	}
 
 	@Override
@@ -208,21 +156,6 @@ public class BaseTestResult implements TestResult {
 		sb.append(encodedTestName);
 
 		return sb.toString();
-	}
-
-	@Override
-	public boolean hasLiferayLog(String testrayLogsURL) {
-		String liferayLog = null;
-
-		try {
-			liferayLog = JenkinsResultsParserUtil.toString(
-				getLiferayLogURL(testrayLogsURL), false, 0, 0, 0);
-		}
-		catch (IOException ioe) {
-			return false;
-		}
-
-		return !liferayLog.isEmpty();
 	}
 
 	protected Build build;
