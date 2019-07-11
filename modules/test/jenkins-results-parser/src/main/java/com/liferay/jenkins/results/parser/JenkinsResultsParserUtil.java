285f15d6a67ed2434ca026922e35b6a62e22327f
Kenji Heigel <kenji.heigel@liferay.com>
2019-07-11 13:38:03 -0700

    Revert "TEMP Append k8s DNS suffix to Jenkins slave name"
    This reverts commit 19e571ba74f72dade2916fc552316e07a7f2983f.

--- modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/JenkinsResultsParserUtil.java
+++ modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/JenkinsResultsParserUtil.java
@@ -1532,12 +1532,6 @@ public class JenkinsResultsParserUtil {
 					buildProperties.getProperty(propertyName.toString()));
 
 				for (String slave : slavesString.split(",")) {
-					slave = slave.trim();
-
-					if (slave.startsWith("slave-ci-")) {
-						slave = slave + ".ssh-svc.ci-1.svc.cluster.local";
-					}
-
 					slaves.add(slave.trim());
 				}
 			}
