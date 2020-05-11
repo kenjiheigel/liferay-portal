08c73114c46b1b909bc49cef2a184a8232965cb3
Kenji Heigel <kenji.heigel@liferay.com>
2020-05-11 13:01:40 -0700

    TEMP use test-5-1 for upstream result

--- modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/UpstreamFailureUtil.java
+++ modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/UpstreamFailureUtil.java
@@ -190,8 +190,7 @@ public class UpstreamFailureUtil {
 
 		String url = JenkinsResultsParserUtil.getLocalURL(
 			JenkinsResultsParserUtil.combine(
-				_URL_BASE_UPSTREAM_FAILURES_JOB,
-				jobName.replace("pullrequest", "upstream-dxp"),
+				_URL_BASE_UPSTREAM_FAILURES_JOB, jobName,
 				"/builds/latest/test.results.json"));
 
 		try {
@@ -276,7 +275,7 @@ public class UpstreamFailureUtil {
 	}
 
 	private static final String _URL_BASE_UPSTREAM_FAILURES_JOB =
-		"https://test-1-0.liferay.com/userContent/testResults/";
+		"https://test-5-1.liferay.com/userContent/testResults/";
 
 	private static boolean _upstreamComparisonAvailable = true;
 	private static JSONObject _upstreamFailuresJobJSONObject;
