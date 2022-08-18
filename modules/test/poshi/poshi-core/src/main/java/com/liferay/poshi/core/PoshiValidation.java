84d45aff56d31f5acd8d56ba0fe0190987a86002
CalumR23 <searagan@gmail.com>
2022-08-18 15:14:22 -0700

    POSHI-357 Add validation for unescaped quotes in vars

--- modules/test/poshi/poshi-core/src/main/java/com/liferay/poshi/core/PoshiValidation.java
+++ modules/test/poshi/poshi-core/src/main/java/com/liferay/poshi/core/PoshiValidation.java
@@ -1779,6 +1779,11 @@ public class PoshiValidation {
 
 		validatePossibleAttributeNames(poshiElement, possibleAttributeNames);
 
+		if (Validator.isNotNull(poshiElement.attributeValue("value"))) {
+			validateAttributeValue(
+				poshiElement, poshiElement.attributeValue("value"));
+		}
+
 		if (Validator.isNotNull(poshiElement.attributeValue("method"))) {
 			String methodAttributeValue = poshiElement.attributeValue("method");
 
