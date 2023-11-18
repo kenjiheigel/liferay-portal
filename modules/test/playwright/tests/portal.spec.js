efd8d8e101570d7f61d96201d273efbb974e7d25
Kenji Heigel <kenji.heigel@liferay.com>
2023-11-17 16:35:20 -0800

    LRCI-3881 Auto SF

--- modules/test/playwright/tests/portal.spec.js
+++ modules/test/playwright/tests/portal.spec.js
@@ -1,7 +1,13 @@
-const { test, expect } = require('@playwright/test');
+/**
+ * SPDX-FileCopyrightText: (c) 2000 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
 
-test('has title', async ({ page }) => {
-  await page.goto(process.env.PORTAL_URL);
-  await expect(page).toHaveTitle("Home - Liferay DXP");
-  await expect(page.locator('#main-content img')).toBeVisible();
+const {expect, test} = require('@playwright/test');
+
+test('has title', async ({page}) => {
+	await page.goto(process.env.PORTAL_URL);
+
+	await expect(page).toHaveTitle('Home - Liferay DXP');
+	await expect(page.locator('#main-content img')).toBeVisible();
 });
