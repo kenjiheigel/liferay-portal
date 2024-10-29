0f739615f792f6413d7a9a22f65ef20c9d5d6cf9
Kenji Heigel <kenji.heigel@liferay.com>
2024-10-29 14:31:21 -0700

    LRCI-4871 Use specific function instead of calling main

--- modules/test/playwright/tests/client-extension-web/env/set_up.sh
+++ modules/test/playwright/tests/client-extension-web/env/set_up.sh
@@ -6,13 +6,11 @@ echo CURRENT_DIR_NAME=${CURRENT_DIR_NAME}
 
 source ${CURRENT_DIR_NAME}/../../../env/common.sh
 
-main "${@}"
-
 function main {
   combine_properties_files \
     $(get_tomcat_portal_ext_properties_file) \
     \
-    ${_PORTAL_PROJECT_DIR}/workspaces/liferay-sample-workspace/configs/local/portal-ext.properties
+    $(get_portal_project_dir)/workspaces/liferay-sample-workspace/configs/local/portal-ext.properties
 
 	default_set_up
 }
