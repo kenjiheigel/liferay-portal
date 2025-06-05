de74a3445a314d985016b09ecc56ca195e5027ca
Kenji Heigel <kenji.heigel@liferay.com>
2025-06-05 10:46:18 -0700

    LRCI-5844 Allow passing in args to CloudBucketUtil.listGCPFiles

--- modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/CloudBucketUtil.java
+++ modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/CloudBucketUtil.java
@@ -265,12 +265,22 @@ public class CloudBucketUtil {
 		return false;
 	}
 
-	public static String listGCPFiles(String path)
+	public static String listGCPFiles(String path, String... args)
 		throws IOException, TimeoutException {
 
+		StringBuilder sb = new StringBuilder();
+
+		sb.append("gcloud storage ls ");
+
+		for (String arg : args) {
+			sb.append(arg);
+			sb.append(" ");
+		}
+
+		sb.append(_escapeParentheses(path));
+
 		Process process = JenkinsResultsParserUtil.executeBashCommands(
-			true, _getGCPAuthenticationCommand(path, path),
-			"gcloud storage ls " + _escapeParentheses(path));
+			true, _getGCPAuthenticationCommand(path, path), sb.toString());
 
 		return JenkinsResultsParserUtil.readInputStream(
 			process.getInputStream());
