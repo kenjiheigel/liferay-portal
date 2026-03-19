a619510e2c1f7d61d3f9ef5b78c67ba796fe8de8
Kenji Heigel <kenji.heigel@liferay.com>
2026-03-18 19:29:37 -0700

    LRCI-7139 Let the modifiedModuleDirsList be cacheable in PortalGitWorkingDirectory

--- modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/PortalGitWorkingDirectory.java
+++ modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/PortalGitWorkingDirectory.java
@@ -62,7 +62,13 @@ public class PortalGitWorkingDirectory extends GitWorkingDirectory {
 	}
 
 	public List<File> getModifiedModuleDirsList() throws IOException {
-		return getModifiedModuleDirsList(null, null);
+		if (_modifiedModuleDirs != null) {
+			return _modifiedModuleDirs;
+		}
+
+		_modifiedModuleDirs = getModifiedModuleDirsList(null, null);
+
+		return _modifiedModuleDirs;
 	}
 
 	public List<File> getModifiedModuleDirsList(
@@ -70,9 +76,22 @@ public class PortalGitWorkingDirectory extends GitWorkingDirectory {
 			List<PathMatcher> includesPathMatchers)
 		throws IOException {
 
-		return JenkinsResultsParserUtil.getDirectoriesContainingFiles(
-			getModuleDirsList(excludesPathMatchers, includesPathMatchers),
-			getModifiedFilesList());
+		if ((excludesPathMatchers == null) && (includesPathMatchers == null) &&
+			(_modifiedModuleDirs != null)) {
+
+			return _modifiedModuleDirs;
+		}
+
+		List<File> modifiedModuleDirsList =
+			JenkinsResultsParserUtil.getDirectoriesContainingFiles(
+				getModuleDirsList(excludesPathMatchers, includesPathMatchers),
+				getModifiedFilesList());
+
+		if ((excludesPathMatchers == null) && (includesPathMatchers == null)) {
+			_modifiedModuleDirs = modifiedModuleDirsList;
+		}
+
+		return modifiedModuleDirsList;
 	}
 
 	public List<File> getModifiedNonposhiModules() throws IOException {
@@ -537,6 +556,7 @@ public class PortalGitWorkingDirectory extends GitWorkingDirectory {
 
 	private Properties _appServerProperties;
 	private List<File> _jsUnitFiles;
+	private List<File> _modifiedModuleDirs;
 	private Properties _releaseProperties;
 	private Properties _testProperties;
 
