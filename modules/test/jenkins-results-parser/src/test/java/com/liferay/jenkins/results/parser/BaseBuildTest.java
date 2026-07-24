28978f447d4f15414b4ed6fb21feb1cd792f1544
Kenji Heigel <kenji.heigel@liferay.com>
2026-07-24 15:23:19 -0700

    LRCI-7941 Add unit tests for the bad build URL list and the save guard
    Reverting the guards fails both tests, the first with the reported NullPointerException. ReflectionTestUtil reaches the private invocation list, the way JenkinsMasterTestUtil already does, so a mocked build carries unresolvable invocations without running the constructor.

---
+++ modules/test/jenkins-results-parser/src/test/java/com/liferay/jenkins/results/parser/BaseBuildTest.java
@@ -0,0 +1,101 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2026 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.jenkins.results.parser;
+
+import java.util.Arrays;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import org.mockito.Mockito;
+
+/**
+ * @author Kenji Heigel
+ */
+public class BaseBuildTest extends com.liferay.jenkins.results.parser.Test {
+
+	@Test
+	public void testGetBadBuildURLs() {
+		BaseBuild baseBuild = Mockito.mock(BaseBuild.class);
+
+		Build.Invocation firstInvocation = new Build.Invocation(baseBuild);
+		Build.Invocation lastInvocation = new Build.Invocation(baseBuild);
+
+		String firstBuildURL = "https://test-1-1.liferay.com/job/test/1/";
+
+		firstInvocation.setBuildURL(firstBuildURL);
+
+		lastInvocation.setBuildURL("https://test-1-1.liferay.com/job/test/3/");
+
+		ReflectionTestUtil.setFieldValue(
+			baseBuild, "_invocations",
+			Arrays.asList(
+				firstInvocation, new Build.Invocation(baseBuild),
+				lastInvocation));
+
+		Mockito.doCallRealMethod(
+		).when(
+			baseBuild
+		).getBadBuildURLs();
+
+		Assert.assertEquals(
+			Arrays.asList(firstBuildURL), baseBuild.getBadBuildURLs());
+	}
+
+	@Test
+	public void testSaveBuildURLInBuildDatabase() {
+		_testSaveBuildURLInBuildDatabase(
+			RandomTestUtil.randomString(), RandomTestUtil.randomString(), true);
+		_testSaveBuildURLInBuildDatabase(
+			RandomTestUtil.randomString(), null, false);
+		_testSaveBuildURLInBuildDatabase(
+			null, RandomTestUtil.randomString(), false);
+	}
+
+	private void _testSaveBuildURLInBuildDatabase(
+		String buildURL, String jobVariant, boolean saved) {
+
+		BaseBuild baseBuild = Mockito.mock(BaseBuild.class);
+		BuildDatabase buildDatabase = Mockito.mock(BuildDatabase.class);
+
+		Mockito.when(
+			baseBuild.getBuildDatabase()
+		).thenReturn(
+			buildDatabase
+		);
+
+		Mockito.when(
+			baseBuild.getBuildURL()
+		).thenReturn(
+			buildURL
+		);
+
+		Mockito.when(
+			baseBuild.getJobVariant()
+		).thenReturn(
+			jobVariant
+		);
+
+		Mockito.doCallRealMethod(
+		).when(
+			baseBuild
+		).saveBuildURLInBuildDatabase();
+
+		baseBuild.saveBuildURLInBuildDatabase();
+
+		if (saved) {
+			Mockito.verify(
+				buildDatabase
+			).putProperty(
+				BaseBuild.BUILD_URLS_PROPERTIES_KEY, jobVariant, buildURL, false
+			);
+		}
+		else {
+			Mockito.verifyNoInteractions(buildDatabase);
+		}
+	}
+
+}
